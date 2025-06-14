eb351d6dd6a1a35c7d21f8d428d84552ef632059
metaa35 <tarhanmert00@gmail.com>
2025-06-15 02:45:24 +0300

    NextAuth tip genişletmesi ile session.user.id ve role eklendi

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,20 +1,22 @@
-import { DefaultSession } from 'next-auth'
-import { Role } from '@prisma/client'
+import NextAuth, { DefaultSession } from "next-auth"
+import { Role } from "@prisma/client"
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface Session {
     user: {
+      id: string
       role: Role
-    } & DefaultSession['user']
+    } & DefaultSession["user"]
   }
-
   interface User {
+    id: string
     role: Role
   }
 }
 
-declare module 'next-auth/jwt' {
+declare module "next-auth/jwt" {
   interface JWT {
+    id: string
     role: Role
   }
 } 
