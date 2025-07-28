2c35a3022def2dc8ca768fc51a684b04dfa134f4
Meta <meta@example.com>
2025-07-28 23:55:58 +0300

    Telefon ve adres bilgileri güncellendi

--- src/app/iletisim/page.tsx
+++ src/app/iletisim/page.tsx
@@ -145,7 +145,7 @@ export default function ContactPage() {
                     <div>
                       <h4 className="text-lg font-semibold mb-1 text-[#2D3436]">Adres</h4>
                       <p className="text-[#636E72]">
-                        İstanbul, Türkiye
+                        Bozköy Mah. Sanayi Cad. Yeni Foça Yolu Üzeri No:9 Aliağa/İzmir
                       </p>
                     </div>
                   </div>
@@ -171,7 +171,7 @@ export default function ContactPage() {
                     <div>
                       <h4 className="text-lg font-semibold mb-1 text-[#2D3436]">Telefon</h4>
                       <p className="text-[#636E72]">
-                        +90 (212) 123 45 67
+                        +90 (532) 717 4087 - +90 (532) 729 6372
                       </p>
                     </div>
                   </div>
@@ -224,20 +224,12 @@ export default function ContactPage() {
           </div>
         </div>
       </section>
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3829.567080735649!2d26.9604358087369!3d38.73426560502259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sBozk%C3%B6y%20Mah.%20Sanayi%20Cad.%20Yeni%20Fo%C3%A7a%20Yolu%20%C3%9Czeri%20No%3A9%20!5e1!3m2!1str!2str!4v1753736086376!5m2!1str!2str" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       {/* Map Section */}
       <section className="py-20 bg-white">
         <div className="container mx-auto px-4">
           <div className="aspect-video relative rounded-2xl overflow-hidden">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d385398.5897809314!2d28.731994399999998!3d41.0049823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caa7040068086b%3A0xe1ccfe98bc01b0d0!2zxLBzdGFuYnVs!5e0!3m2!1str!2str!4v1647881234567!5m2!1str!2str"
-              width="100%"
-              height="100%"
-              style={{ border: 0 }}
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            />
+          <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3829.567080735649!2d26.9604358087369!3d38.73426560502259!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sBozk%C3%B6y%20Mah.%20Sanayi%20Cad.%20Yeni%20Fo%C3%A7a%20Yolu%20%C3%9Czeri%20No%3A9%20!5e1!3m2!1str!2str!4v1753736086376!5m2!1str!2str" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </section>
